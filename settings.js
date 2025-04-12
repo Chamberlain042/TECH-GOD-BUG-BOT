358e87cc0e0fc743bf91fe79095141f583dafcf2
Chamberlain042 <166442989+Chamberlain042@users.noreply.github.com>
2025-04-12 08:01:13 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,10 +38,11 @@ global.prefa = ['','!','.','#','&']
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
-global.autoread = false //auto read messages
+global.autoread = true //auto read messages
 global.autobio = true //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
+global.autosreact = true //auto react status 
 
 //menu type 
 //v1 is image menu, 
